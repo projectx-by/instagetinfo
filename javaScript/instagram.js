efaa2bcbd80c72e06721d28b49026d619af9570e
@code <@code>
2023-05-15 12:35:17 +0700

    cleanCode 1.0

--- javaScript/instagram.js
+++ javaScript/instagram.js
@@ -233,7 +233,6 @@ function childPostList(resPostOrder, modalId) {
   </div>
 </div>`;
   containerResult.insertBefore(divModal, footerResult);
-  console.log(`post ke ${modalId} has childpost ${resPostOrder.length}`);
   for (indexChild = 0; indexChild < resPostOrder.length; indexChild++) {
     // console.log(resPostOrder[indexChild].node.display_url);
     let slideContent = document.createElement('div');
@@ -245,23 +244,9 @@ function childPostList(resPostOrder, modalId) {
                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                    <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
                  </svg></a></div>`;
-
-      // `<div class="d-flex justify-content-center my${modalId}"><a href="${resPostOrder[indexChild].node.video_url}" target="_blank" class="text-decoration-none text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
-      //             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-      //             <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-      //           </svg></a></div>`
     }
     document.getElementById(`container-slide${modalId}`).appendChild(slideContent);
   }
-  // let pBtnControl = document.createElement('p');
-  // pBtnControl.innerHTML = `<a class="prev-button text-decoration-none bg-dark rounded-end text-white" onclick="plusSlides(-1,${modalId})"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-compact-left" viewBox="0 0 16 16">
-  //         <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z" />
-  //       </svg></a>
-  //       <a class="next-button text-decoration-none bg-dark rounded-start text-white" onclick="plusSlides(1,${modalId})"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-compact-right" viewBox="0 0 16 16">
-  //         <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z" />
-  //       </svg></a>`;
-  // document.getElementById(`container-slide${modalId}`).appendChild(pBtnControl);
-  // console.log(document.getElementById(`container-slide${modalId}`).children);
 }
 function modalVideo(postOrder, modalId) {
   let divModal = document.createElement('div');
@@ -277,12 +262,7 @@ function modalVideo(postOrder, modalId) {
             </div>
           </div>
         </div>
-  //     </div>`;
-  // `<div class="mySlides my-fade my${modalId} text-center" style="position:relative;"><div class="text-dark fs-5">${indexChild + 1} / ${resPostOrder.length}</div><img src="${resPostOrder[indexChild].node.display_url}" style="width:85%;" alt="post-child-img" crossorigin="anonymous"><a href="${resPostOrder[indexChild].node.video_url}" target="_blank" class="text-decoration-none text-white" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);"><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
-  //                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-  //                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-  //                </svg></a></div>`
-
+     </div>`;
   containerResult.insertBefore(divModal, footerResult);
 }
 let slideIndex = 1;
@@ -295,14 +275,11 @@ function currentSlide(n, slideOfPost) {
 }
 function showSlides(n, slideOfPost) {
   let slides = document.getElementsByClassName(`my${slideOfPost} `);
-  console.log(slides)
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    console.log(slides[i])
   }
-  console.log(slides[slideIndex - 1])
   slides[slideIndex - 1].style.display = "block";
 
 }
@@ -356,41 +333,6 @@ function showSlides(n, slideOfPost) {
 // }
 // test();
 
-
-/* <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div> */
-
-/* <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>`; */
-// let bodyChildPost = document.createElement('div');
-// bodyChildPost.setAttribute('class','collapse');
-// bodyChildPost.setAttribute('style','border:none;outline:none;');
-// bodyChildPost.setAttribute('id',`body-child-post-${postOrder}`);
-// bodyChildPost.innerHTML = `<div class="card card-body bg-dark text-white d-flex justify-content-between" id="child-post-list" style="border:none;outline:none;">
-//     <img src="../asset/favicon.ico" class="img-fluid my-1 mx-1" alt="post-images" style="width: 1w ! important; height: 1w !important;" crossorigin="anonymous">
-//     <img src="../asset/favicon.ico" class="img-fluid my-1 mx-1" alt="post-images" style="width: 1w ! important; height: 1w !important;" crossorigin="anonymous">
-//     <img src="../asset/favicon.ico" class="img-fluid my-1 mx-1" alt="post-images" style="width: 1w ! important; height: 1w !important;" crossorigin="anonymous">
-//   </div>`;
-//   document.getElementById(`card-body-${postOrder}`).appendChild(bodyChildPost);
-//   let btnDetails = document.getElementById(`details-${postOrder}`);
-//   btnDetails.setAttribute('data-bs-toggle','collapse');
-//   btnDetails.setAttribute('data-bs-target',`#body-child-post-${postOrder}`);
-//   btnDetails.setAttribute('aria-expanded','false');
-//   btnDetails.setAttribute('aria-controls',`body-child-post-${postOrder}`);
-
 function modalProfile(res) {
   let popupModal = document.createElement('div');
   popupModal.setAttribute('class', 'modal');
@@ -427,8 +369,6 @@ function bodyClick(event) {
     document.body.classList.remove('all-blur');
     slideIndex = 1;
   }
-  console.log(event)
-  // if (event.target.)
 }
 function keyCode(event) {
   if (event.keyCode == 27) document.body.classList.remove('all-blur');
@@ -453,8 +393,6 @@ function loadingEffect() {
   containerResult.insertBefore(loadingEvent, footerResult);
 }
 
-
-
 let userInfo = {
   method: 'POST',
   mode: 'cors',
@@ -466,6 +404,7 @@ let userInfo = {
     'X-RapidAPI-Host': 'rocketapi-for-instagram.p.rapidapi.com'
   }
 };
+
 function getInfo(url, user) {
   return fetch(url, user)
     .then(response => response.json())
